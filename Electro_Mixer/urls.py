033c5fd5aea985acbc64eeaf5f3f39e126e716d7
Marco Pazol <donjulionlimon@gmail.com>
2024-01-03 16:57:06 -0600

    v0.0.2 - urls.py created on Electro_Mixer_App directory

--- Electro_Mixer/urls.py
+++ Electro_Mixer/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Electro_Mixer_App.urls import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', home, name = 'home'),
 ]
